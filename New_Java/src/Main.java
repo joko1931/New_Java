bc5e0e22605b521ab2d30b2cd59052508991d88f
Joel O'Koon <144722950+joko1931@users.noreply.github.com>
2023-09-12 15:04:39 -0400

    Add files via upload

---
+++ New_Java/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int age = 16;
+        String greeting = "Hello Joe, watta ya know?";
+
+        System.out.println(greeting);
+        System.out.println(greeting + " Got beer?!");
+    }   // return
+}
